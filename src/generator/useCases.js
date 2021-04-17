118e2dbc90116e3f8310cb720e55c02edab448e0
italojs <italo.i@live.com>
2021-04-17 19:36:32 -0300

    returning request as string

--- src/generator/useCases.js
+++ src/generator/useCases.js
@@ -1,22 +1,24 @@
-async function prepareRequest(schema){
-    const obj = {}
-    Object.keys(schema).map(key => {
+async function requestString(schema){
+    const obj = Object.keys(schema).reduce((obj, key) => {
         const { name, type } = schema[key]
-        obj[name] = type
-    }
+        obj[name] = type.name;
+        return obj;
+      }, {});
+    return JSON.stringify(obj).replaceAll('"', '')
 }
+
 module.exports =  async ({ generate, filesystem }) => async () => {
     const entities = require(`${filesystem.cwd()}/src/domain/entities`)
     for(const key of Object.keys(entities)){
         const { name, schema } = entities[key].prototype.meta
         
-        
+        console.log()
         await generate({
             template: 'usecases/useCase.ejs',
             target: `src/domain/useCases/create${name}.js`,
             props: { 
                 entity: name,
-                request: JSON.stringify(schema)
+                request: await requestString(schema)
              },
         })
     }
