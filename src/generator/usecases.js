6a6318a92b4792fc7e137e26e8329515be3894b5
italojs <italo.i@live.com>
2021-04-21 15:35:18 -0300

    removed complexity for example generation

--- src/generator/usecases.js
+++ src/generator/usecases.js
@@ -1,15 +1,16 @@
-const defaultValues = {
-    String: '\'\'',
-    Number: 100,
-    Boolean: true,
-    Date: new Date()
-}
-
 const useCases = ['create', 'update', 'delete']
 
-async function getPropertiesStr(obj, spaces){
+
+async function generateRequest (schema){
+    // schema to plain JSON
+    const obj = Object.keys(schema).reduce((obj, key) => {
+        const { name, type } = schema[key]
+        obj[name] = type.name || type.constructor.name;
+        return obj;
+    }, {});
+
     //convert plain JSON and remove quotation marks(")
-    const str = JSON.stringify(obj, null, spaces)
+    const str = JSON.stringify(obj, null, 8)
     .replaceAll('"', '')
     .split('\n')
 
@@ -19,29 +20,12 @@ async function getPropertiesStr(obj, spaces){
 
     return str.join('\n').trim()
 }
-async function objToStr(schema, proType, spaces) {
-    // schema to plain JSON
-    const obj = Object.keys(schema).reduce((obj, key) => {
-        const { name, type } = schema[key]
-        if(proType === 'request') obj[name] = type.name || type.constructor.name;
-        if(proType === 'example') obj[name] = defaultValues[type.name]
-        return obj;
-    }, {});
-    return getPropertiesStr(obj, spaces)
-}
-async function propGenerator (schema){
-    return {
-        request: () => objToStr(schema, 'request', 6),
-        example: () => objToStr(schema, 'example', 8)
-    }
-}
 
 module.exports =  async ({ generate, filesystem }) => async () => {
     const entities = require(`${filesystem.cwd()}/src/domain/entities`)
     for(const entity of Object.keys(entities)){
         const { name, schema } = entities[entity].prototype.meta
-        const props = await propGenerator(schema)
-   
+
         for(const action of useCases){
             await generate({
                 template: `usecases/${action}.ejs`,
@@ -51,8 +35,7 @@ module.exports =  async ({ generate, filesystem }) => async () => {
                         pascalCase: name,
                         camelCase: `${name[0].toLowerCase()}${name.slice(1)}`
                     },
-                    request: await props.request(),
-                    example: await props.example()
+                    request: await generateRequest(schema)
                 }
             })
         }
