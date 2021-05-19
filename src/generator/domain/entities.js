8b2c4ff73ee62ad214f891e9a157252534664d86
Daniel Sousa <danieldelphi2013@gmail.com>
2021-05-19 12:42:53 -0300

    Reading the entities provided by the user

--- src/generator/domain/entities.js
+++ src/generator/domain/entities.js
@@ -1,31 +1,39 @@
 const { toLowCamelCase, objToString } = require('../utils')
+const { filesystem } = require('gluegun')
+// const regex = /(entity\(.[a-z].+(\s.+){0,}\}\))/gim
 
-let entities = [
-    {
-        name: "User",
-        fields: `nickname: field(String),
+const entities = [
+  {
+    name: 'User',
+    fields: `nickname: field(String),
             password: field(String)`
-    }
+  }
 ]
-module.exports = async ({ generate, options } ) => async () => {
-    if (options.entities){
-        // TODO: BUILD CUSTOM ENTITIES
-    }
-    const requires = {}
-    await Promise.all(entities.map(async entity => {
-        const nameInCC = toLowCamelCase(entity.name)
-        
-        await generate({
-            template: `domain/entities/${nameInCC}.ejs`,
-            target: `src/domain/entities/${nameInCC}.js`,
-            props: entity
-        })
-        requires[entity.name] = `require('./${nameInCC}.js')` 
-    }))
-
-    await generate({
-        template: 'domain/entities/index.ejs',
-        target: `src/domain/entities/index.js`,
-        props: { requires: objToString(requires) }
+module.exports = async ({ generate, options }) => async () => {
+  const requires = {}
+  if (options.entities && options.entities !== true) {
+    const files = await filesystem.list(options.entities)
+    files.forEach((file) => {
+      filesystem.copy(`${options.entities}\\\\${file}`, `${filesystem.cwd()}/src/domain/entities/${file}`)
+      const entity = require(`${filesystem.cwd()}/src/domain/entities/${file}`)
+      requires[entity.name] = `require('./${file}')`
     })
+  } else {
+    await Promise.all(entities.map(async entity => {
+      const nameInCC = toLowCamelCase(entity.name)
+
+      await generate({
+        template: `domain/entities/${nameInCC}.ejs`,
+        target: `src/domain/entities/${nameInCC}.js`,
+        props: entity
+      })
+      requires[entity.name] = `require('./${nameInCC}.js')`
+    }))
+  }
+
+  await generate({
+    template: 'domain/entities/index.ejs',
+    target: 'src/domain/entities/index.js',
+    props: { requires: objToString(requires) }
+  })
 }
