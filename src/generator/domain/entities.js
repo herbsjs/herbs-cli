79777a1229328c01d71c4d3d233c4b23bcaf4938
Daniel Sousa <danieldelphi2013@gmail.com>
2021-05-21 21:24:33 -0300

    refactoring to use generate entities in a generic way

--- src/generator/domain/entities.js
+++ src/generator/domain/entities.js
@@ -1,34 +1,29 @@
-const { toLowCamelCase, objToString } = require('../utils')
+const { objToString } = require('../utils')
 const { filesystem } = require('gluegun')
-// const regex = /(entity\(.[a-z].+(\s.+){0,}\}\))/gim
+const { join } = require('path')
 
-const entities = [
-  {
-    name: 'User',
-    fields: `nickname: field(String),
-            password: field(String)`
-  }
-]
-module.exports = async ({ generate, options }) => async () => {
+const generateEntities = (dir) => {
   const requires = {}
-  if (options.entities && options.entities !== true) {
-    const files = await filesystem.list(options.entities)
-    files.forEach((file) => {
-      filesystem.copy(`${options.entities}\\\\${file}`, `${filesystem.cwd()}/src/domain/entities/${file}`)
+  const files = filesystem.list(dir)
+
+  Promise.all(files.map((file) => {
+    filesystem.copy(`${dir}/${file}`, `${filesystem.cwd()}/src/domain/entities/${file}`, { matching: '*.js' })
+    const isJS = filesystem.inspect(`${filesystem.cwd()}/src/domain/entities/${file}`)
+    if (isJS) {
       const entity = require(`${filesystem.cwd()}/src/domain/entities/${file}`)
       requires[entity.name] = `require('./${file}')`
-    })
-  } else {
-    await Promise.all(entities.map(async entity => {
-      const nameInCC = toLowCamelCase(entity.name)
+    }
+  }))
 
-      await generate({
-        template: `domain/entities/${nameInCC}.ejs`,
-        target: `src/domain/entities/${nameInCC}.js`,
-        props: entity
-      })
-      requires[entity.name] = `require('./${nameInCC}.js')`
-    }))
+  return requires
+}
+
+module.exports = async ({ generate, options }) => async () => {
+  let requires = {}
+  if (options.entities && options.entities !== true) {
+    requires = await generateEntities(options.entities)
+  } else {
+    requires = await generateEntities(join(__dirname, '../../templates/domain/entities'))
   }
 
   await generate({
