91249e79d039c174e6fc260ee900774655cfd3d5
Pedro Marques Freire <pedromarques1011@gmail.com>
2021-06-25 14:16:37 -0300

    correcao

--- src/generator/src/infra/server.js
+++ src/generator/src/infra/server.js
@@ -1,5 +1,4 @@
 module.exports = async ({ generate, options }) => async () => {
-  console.log(options)
   await generate({
     template: 'infra/api/server.ejs',
     target: `src/infra/api/server.js`,
