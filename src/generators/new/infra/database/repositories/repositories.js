42a914389589e6f96c3d9429abc1068681ea0326
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2021-09-30 12:24:05 -0300

    refactor: generating repositories of new entities with the update command

--- src/generators/new/infra/database/repositories/repositories.js
+++ src/generators/new/infra/database/repositories/repositories.js
@@ -1,7 +1,7 @@
 const { objToString } = require('../../../../utils')
 const camelCase = require('lodash.camelcase')
 const fs = require('fs')
-async function generateRepositories (generate, filesystem, db) {
+async function generateRepositories(generate, filesystem, db) {
   const requires = {}
   const entities = require(`${filesystem.cwd()}/src/domain/entities`)
 
@@ -29,27 +29,26 @@ async function generateRepositories (generate, filesystem, db) {
   return requires
 }
 
-async function updateRepositories (generate, filesystem) {
-  const mongoPath = '/src/infra/data/repositories/baseRepository.js'
-  const msSqlPath = '/src/infra/config/sqlserver.js'
-  const pgPath = '/src/infra/config/postgres.js'
-  let db 
+async function updateRepositories(generate, filesystem) {
+  const paths = {
+    mongo: '/src/infra/data/repositories/baseRepository.js',
+    sqlserver: '/src/infra/config/sqlserver.js',
+    postgres: '/src/infra/config/postgres.js'
+  }
 
-  if (fs.existsSync(`${filesystem.cwd()}${mongoPath}`)) db = 'mongo'
-  if (fs.existsSync(`${filesystem.cwd()}${msSqlPath}`)) db = 'sqlserver'
-  if (fs.existsSync(`${filesystem.cwd()}${pgPath}`)) db = 'postgres'
+  const db = Object.keys(paths).filter(key => fs.existsSync(`${filesystem.cwd()}${paths[key]}`))
 
   return generateRepositories(generate, filesystem, db)
 }
 
 module.exports = async ({ template: { generate }, parameters: { options }, filesystem }, isUpdate) => async () => {
   let requires = {}
-  
+
   if (isUpdate) requires = await updateRepositories(generate, filesystem)
 
   for (const db of ['postgres', 'sqlserver', 'mongo']) {
     if (!options[db]) continue
-    
+
     if (db === 'mongo') {
       await generate({
         template: 'data/repository/mongo/baseRepository.ejs',
