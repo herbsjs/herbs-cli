00736eff8d88d4e3492f213c0a46937d7a09b0fc
cassiosilva93 <cos@vortx.com.br>
2022-01-23 12:27:08 -0300

    chore: create function to generate random project name

--- helpers/generate-random-project-name.js
+++ helpers/generate-random-project-name.js
@@ -1,6 +1,6 @@
 const generateRandomProjectName = () => {
-  const name = `${Math.random().toString(36).substring(7)}-project`;
-  return name.toLowerCase();
-};
+  const name = `${Math.random().toString(36).substring(7)}-project`
+  return name.toLowerCase()
+}
 
-module.exports = generateRandomProjectName;
+module.exports = generateRandomProjectName
