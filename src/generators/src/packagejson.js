7759695ab26a4209ac6b429d4738798702c8e716
David Lojudice Sobrinho <dalssoft@gmail.com>
2022-02-23 10:17:35 -0300

    fix(graphql): dont include packages related to graphql when it's not selected

--- src/generators/src/packagejson.js
+++ src/generators/src/packagejson.js
@@ -7,7 +7,7 @@ const optionalPackages = {
   sqlserver: ['"@herbsjs/herbs2knex": "^1.4.1"', '"tedious": "^14.0.0"'],
   mysql: ['"@herbsjs/herbs2knex": "^1.4.1"', '"mysql2": "^2.3.3"'],
   rest: ['"express": "^4.17.2"', '"cors": "^2.8.5"', '"@herbsjs/herbs2rest": "^2.0.1"'],
-  graphql: ['"graphql": "^15.8.0"', '"apollo-server": "^3.6.1"',
+  graphql: ['"graphql": "^15.8.0"', '"@herbsjs/herbs2gql": "^2.0.1"', '"apollo-server": "^3.6.1"',
     '"apollo-server-express": "^3.6.1"', '"graphql-tools": "^8.2.0"', '"graphql-scalars": "^1.14.1"',]
 }
 
@@ -48,7 +48,6 @@ module.exports =
       let packages = [
         '"@herbsjs/herbs": "^1.4.2"',
         '"@herbsjs/herbarium": "^1.1.0"',
-        '"@herbsjs/herbs2gql": "^2.0.1"',
         '"@herbsjs/herbsshelf": "^2.1.2"',
         '"deepmerge": "^4.2.2"',
         '"dotenv": "^12.0.3"',
