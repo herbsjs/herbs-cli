d437dadf25920549cd5486bf184e1ad68e10263e
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2022-05-17 01:22:59 -0300

    refactor: validate git config

--- src/generators/src/infra/git.js
+++ src/generators/src/infra/git.js
@@ -1,15 +1,23 @@
+
 module.exports =
   async ({ template: { generate }, system, filesystem }) =>
     async () => {
       const directory = filesystem.cwd()
       const platform = process.platform
-      const { spawnSync } = require('child_process')
+      const { spawnSync, exec } = require('child_process')
       const where = platform === 'win32' ? 'where.exe' : 'which'
       const out = spawnSync(where, ['git'])
+
       if (out.status === 1) {
         process.stdout.write(`[ERROR] Git not found\n install it via https://git-scm.com/downloads and try again, or skip the git option when generates a new project with Herbs-CLI\n`)
       } else {
         process.stdout.write(`Generating git\n`)
+        exec('git config --list', (error) => {
+            if (error) {
+              process.stdout.write(`exec error: ${error}`)
+              return
+            }
+          })
         await generate({
           template: 'infra/config/.gitignore.ejs',
           target: '.gitignore',
