b2da950582ae3366f660fcc1320d08105ae7dee5
David Lojudice Sb <dalssoft@gmail.com>
2022-05-23 23:44:12 -0300

    feat(test): improve test CLI output

--- src/commands/test.js
+++ src/commands/test.js
@@ -14,15 +14,29 @@ const cmd = {
             if (instance.isSpec) specs.push(instance)
         }
 
-        const { grey, green, red } = toolbox.print.colors
-        const passed = 'passed'
+        const { grey, green, red, white, blue, italic } = toolbox.print.colors
+        const failed = 'failed'
         for (const spec of specs) {
             const ret = await spec.run()
             const usecase = spec.usecase().description
-            const result = (ret) => ret === passed ? green('🗸') : red('•')
-            toolbox.print.info(`${grey(usecase)} ${result(ret)}`)
+            const result = (ret) => ret !== failed ? green('🗸') : red('•')
+            toolbox.print.info(`${white(usecase)} ${result(ret)} ${grey(italic('(spec)'))}`)
             for (const scenario of spec.scenarios) {
-                toolbox.print.info(`   ${grey(scenario.description)} ${result(scenario.state)}`)
+                toolbox.print.info(`   ${result(scenario.state)} ${white(scenario.description)} ${grey(italic('(scenario)'))}`)
+                if (scenario.state !== failed) continue
+                const steps = (step) => step.forEach(e => {
+                    const description = e.builtin ? `When run` : e.description
+                    const color = e.state !== failed ? grey : red
+                    const type = `(${e.type})`
+                    toolbox.print.info(`      ${result(e.state)} ${color(description)} ${grey(italic(type))}`)
+                    if (e.error)
+                        toolbox.print.info(`\n          ${blue(e.error)} \n`)
+                })
+                steps(scenario.givens)
+                if (scenario.stage === 'given') continue
+                steps(scenario.whens)
+                if (scenario.stage === 'when') continue
+                steps(scenario.checks)
             }
         }
 
