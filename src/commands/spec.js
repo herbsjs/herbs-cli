2821864f123da101aa6ae43a762c023bbe4ca286
David Lojudice Sb <dalssoft@gmail.com>
2022-06-05 18:39:37 -0300

    feat(spec): new Sample features - be able to add sample data on your scenario

--- src/commands/spec.js
+++ src/commands/spec.js
@@ -14,7 +14,7 @@ const cmd = {
         const { grey, green, red, white, blue, italic } = toolbox.print.colors
         const failed = 'failed'
         let errorCount = 0
-        
+
         for (const specInfo of Array.from(specs.values())) {
             const spec = specInfo.spec
             const ret = await spec.run()
@@ -25,21 +25,36 @@ const cmd = {
             for (const scenario of spec.scenarios) {
                 toolbox.print.info(`   ${result(scenario.state)} ${white(scenario.description)} ${grey(italic('(scenario)'))}`)
                 if (scenario.state !== failed) continue
-                const steps = (step) => step.forEach(e => {
-                    const description = e.builtin ? `When run` : e.description
-                    const color = e.state !== failed ? grey : red
-                    const type = `(${e.type})`
-                    toolbox.print.info(`      ${result(e.state)} ${color(description)} ${grey(italic(type))}`)
-                    if (e.error) {
-                        toolbox.print.info(`\n          ${blue(e.error)} \n`)
-                        errorCount++
+
+                for (const samples of scenario.samples) {
+                    for (const sample of samples.execution.scenarios) {
+                        const prettyPrint = (sample) => {
+                            for (const entrie of Object.entries(sample)) {
+                                toolbox.print.info(`        ${white(`${entrie[0]}: ${entrie[1]}`)}`)    
+                            }
+                        }
+
+                        if (!samples.builtin) {
+                            toolbox.print.info(`   ${white(`${samples.description} ${grey(italic('(sample)'))}:`)}`)
+                            prettyPrint(sample.sample)
+                        }
+                        const steps = (step) => step.forEach(e => {
+                            const description = e.builtin ? `When run` : e.description
+                            const color = e.state !== failed ? grey : red
+                            const type = `(${e.type})`
+                            toolbox.print.info(`      ${result(e.state)} ${color(description)} ${grey(italic(type))}`)
+                            if (e.error) {
+                                toolbox.print.info(`\n          ${blue(e.error)} \n`)
+                                errorCount++
+                            }
+                        })
+                        steps(sample.givens)
+                        if (sample.stage === 'given') continue
+                        steps(sample.whens)
+                        if (sample.stage === 'when') continue
+                        steps(sample.checks)
                     }
-                })
-                steps(scenario.givens)
-                if (scenario.stage === 'given') continue
-                steps(scenario.whens)
-                if (scenario.stage === 'when') continue
-                steps(scenario.checks)
+                }
             }
         }
 
