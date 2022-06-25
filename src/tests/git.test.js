e89f29a62519bfab83c6134ff429e3428762b249
DevDougBrito <douglas.f.brito@gmail.com>
2022-06-25 15:22:27 -0300

    add git fake user name and email

--- src/tests/git.test.js
+++ src/tests/git.test.js
@@ -7,7 +7,8 @@ const path = require('path')
 const projectName = 'herbs-test-runner'
 
 const linknpm = () => system.run(`cd bin && npm link --force`)
-
+const setGitUser = () => system.run(`git config --global user.email "you@example.com"`)
+const setGitEmail = () => system.run(`git config --global user.name "Your Name"`)
 const generateProject = () => system.run(`herbs new --name ${projectName} --description "testing the herbs CLI"  --author herbs --license MIT --graphql --rest --database postgres --git yes --npmInstall no`)
 
 describe('When I generate a complete project that uses git support', () => {
@@ -17,6 +18,8 @@ describe('When I generate a complete project that uses git support', () => {
 
     it('must exists a .gitignore file', async () => {
         await linknpm()
+        await setGitUser()
+        await setGitEmail()
         await generateProject()
         fs.readFileSync(path.resolve(process.cwd(), `${projectName}/.gitignore`))
     })
