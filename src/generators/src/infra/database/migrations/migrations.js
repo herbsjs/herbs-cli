f5be36404904852094b36dad7951d21c336e1e32
Enderson Costa <enderson.tech@gmail.com>
2022-07-05 09:47:41 -0300

    feat: including relationships in migrations

--- src/generators/src/infra/database/migrations/migrations.js
+++ src/generators/src/infra/database/migrations/migrations.js
@@ -2,7 +2,8 @@ const { snakeCase, camelCase } = require('lodash')
 const fs = require("fs")
 const glob = require('glob')
 const path = require('path')
-const { requireHerbarium } = require('../../../../utils')
+const { entity } = require('@herbsjs/gotu')
+const { requireHerbarium, requireHerbs } = require('../../../../utils')
 
 module.exports =
   async ({ template: { generate }, filesystem }, command) =>
@@ -12,6 +13,7 @@ module.exports =
       process.stdout.write(`Generating Migration\n`)
 
       const herbarium = requireHerbarium(command, filesystem.cwd())
+      const herbs = requireHerbs(filesystem.cwd())
       const entities = herbarium.entities.all
 
       const cwd = filesystem.cwd().replace(new RegExp('\\\\', 'g'), '/')
@@ -26,15 +28,11 @@ module.exports =
         }
 
         function type2Str(Type) {
-          const nativeTypes = [Boolean, Number, String, Array, Object, Date, Function]
-
-          if (nativeTypes.includes(Type)) {
-            const _type = new Type()
-            if (_type instanceof String) return 'string'
-            if (_type instanceof Number) return 'integer'
-            if (_type instanceof Boolean) return 'boolean'
-            if (_type instanceof Date) return 'timestamp'
-          }
+          const _type = new Type()
+          if (_type instanceof String) return 'string'
+          if (_type instanceof Number) return 'integer'
+          if (_type instanceof Boolean) return 'boolean'
+          if (_type instanceof Date) return 'timestamp'
         }
 
         function getDBType(appDir) {
@@ -47,9 +45,33 @@ module.exports =
           const columns = []
           Object.keys(schema).forEach((prop) => {
             const { name, type, options } = schema[prop]
-            columns.push(`table.${type2Str(type)}('${snakeCase(name)}')${options.isId ? '.primary()' : ''}`)
+
+            const nativeTypes = [Boolean, Number, String, Date]
+            if (!nativeTypes.includes(type)) return
+
+            const typeString = type2Str(type)
+            columns.push(`table.${typeString}('${snakeCase(name)}')${options.isId ? '.primary()' : ''}`)
           })
           return columns
+        }
+
+        function isArrayWithType(value) {
+          return Array.isArray(value) && value.length === 1
+        }
+
+        function identifyRef(schema) {
+          const refs = []
+          Object.values(schema).forEach(({ type, name }) => {
+            if (herbs.entity.isEntity(type)) {
+              const typeSchema = type.prototype.meta.schema
+              const idRef = Object.values(typeSchema).find(column => column.options.isId)?.name
+              refs.push({ id: idRef, columnName: `${camelCase(name)}Id`, table: `${camelCase(type.name)}s`, relationship: 'One-to-One' })
+            }
+
+            if (isArrayWithType(type) && herbs.entity.isEntity(type[0]))
+              refs.push({ table: `${camelCase(type[0].name)}s`, relationship: 'One-to-Many' })
+          })
+          return refs
         }
 
         const db = getDBType(filesystem.cwd())
@@ -60,11 +82,14 @@ module.exports =
         const migrationFullPath = path.normalize(`${migrationsPath}/${migrationName}_${camelCase(name)}s.js`)
 
         const columns = createColumns(schema)
+        const ref = identifyRef(schema)
+        const idColumn = Object.values(schema).find(column => column.options.isId)?.name
+
 
         await generate({
           template: `infra/data/database/${db.toLowerCase()}/migration.ejs`,
           target: migrationFullPath,
-          props: { table: `${camelCase(name)}s`, columns: columns }
+          props: { table: `${camelCase(name)}s`, externalColumnName: `${camelCase(name)}Id`, columns, ref, idColumn }
         })
         process.stdout.write(`  New: ${migrationFullPath}\n`)
 
