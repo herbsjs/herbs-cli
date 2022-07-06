1618bf04edbe0284c7d90d3a80821e4fed971db6
Daniel Rodrigues Sousa <danieldelphi2013@gmail.com>
2022-07-06 09:42:06 -0300

    feat(component): get permission through parameters

--- src/commands/shell.js
+++ src/commands/shell.js
@@ -1,10 +1,56 @@
 const { requireHerbarium } = require('../generators/utils')
+const inquirer = require('inquirer')
 const repl = require('@herbsjs/herbs2repl')
+
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0
+}
+
+const permissions = [
+  {
+    type: 'confirm',
+    name: 'canCreateItem',
+    message: 'Can you create a item?',
+    default: true,
+  },
+  {
+    type: 'confirm',
+    name: 'canCreateList',
+    message: 'Can you create a list?',
+    default: true,
+  },
+  {
+    type: 'confirm',
+    name: 'canGetLists',
+    message: 'Can you get a lists?',
+    default: true,
+  },
+  {
+    type: 'confirm',
+    name: 'canDeteleList',
+    message: 'Can you delete a list?',
+    default: true,
+  },
+  {
+    type: 'confirm',
+    name: 'canUpdateItem',
+    message: 'Can you update a item?',
+    default: true,
+  },
+  {
+    type: 'confirm',
+    name: 'canUpdateList',
+    message: 'Can you update a list?',
+    default: true,
+  },
+]
 
 const cmd = {
   name: 'shell',
-  description: 'Run Herbs Shell',
+  description: 'Run herbs shell to interact with your project easily',
+  alias: ['sh'],
   run: async toolbox => {
+    let { options } = toolbox.parameters
     const { red } = toolbox.print.colors
     const herbarium = requireHerbarium("shell", toolbox.filesystem.cwd())
 
@@ -15,23 +61,19 @@ const cmd = {
 
     herbarium.requireAll()
 
-    async function main(injection) {
-      const usecases = Array.from(herbarium.usecases.all).map(([_, item]) =>
-          ({ usecase: item.usecase(injection), id: item.id, tags: { group: item.group || "useCase" } }))
+    const usecases = Array.from(herbarium.usecases.all).map(([_, item]) =>
+        ({ usecase: item.usecase(), id: item.id, tags: { group: item.group || "Others" } }))
 
-      const user = {
-          canCreateItem: true, canCreateList: true, canDeteleList: false,
-          canGetLists: true, canUpdateItem: true, canUpdateList: true
-      }
-
-      if (!usecases.length) {
-        toolbox.print.info(`\n ${red('• Exit with error: useCases not found 😢')}`)
-        return
-      }
-
-      repl(usecases, user, { groupBy: "group" })
+    if (!usecases.length) {
+      toolbox.print.info(`\n ${red('• Exit with error: useCases not found 😢')}`)
+      return
     }
-    main().then()
+
+    if (isEmpty(options)) {
+      options = await inquirer.prompt(permissions)    
+    }
+
+    repl(usecases, options, { groupBy: "group" })
   }
 }
 
