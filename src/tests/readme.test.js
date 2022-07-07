da91b9f7c9e105a0c558c8099649e91f52c74160
othonvv <99425672+othonvv@users.noreply.github.com>
2022-07-07 11:45:39 -0300

    Trying new approach
    Trying new approach for readme generation test.

--- src/tests/readme.test.js
+++ src/tests/readme.test.js
@@ -6,39 +6,34 @@ const path = require('path')
 const projectName = 'herbs-readme-test-runner'
 const projectDescription = 'I am test project description'
 
+const linkNpm = () => system.run(`cd bin && npm link --force`)
+const unlinkNpm = () => system.run(`cd bin && npm unlink --force @herbsjs/herbs-cli -g`)
 const npmInstall = () => system.run(`cd ${projectName} && npm install`)
 
-const generateProject = (extraCMDs = '') => system.run(`herbs new --name ${projectName} --description "${projectDescription}" --author herbs --license MIT --graphql --rest --database postgres --npmInstall no ${extraCMDs}`)
+const generateProject = async (extraCMDs = '') => {
+    system.run(`herbs new --name ${projectName} --description "${projectDescription}" --author herbs --license MIT --graphql --rest --database postgres --npmInstall no ${extraCMDs}`)
+}
 
 describe('generates readme file', () => {
-    afterEach(() => {
+    afterEach(async () => {
         fs.rmSync(path.resolve(process.cwd(), `${projectName}`), { recursive: true })
+        await unlinkNpm()
       })
 
-    it('must to use custom database option', async () => {
+    it.only('must to use custom user data', async () => {
 
+        await linkNpm()
         await generateProject()
         await npmInstall()
 
         const pkg = filesystem.read(path.resolve(process.cwd(), `${projectName}/readme.md`))
 
-        const result = pkg.indexOf('Postgres')
-
-        assert.typeOf(pkg, 'string')
-        assert.isAbove(result, -1)
-    }),
-
-    it.only('must to use custom project name and description', async () => {
-
-        await generateProject()
-        await npmInstall()
-
-        const pkg = filesystem.read(path.resolve(process.cwd(), `${projectName}/readme.md`))
-
+        const findProjectDatabase = pkg.indexOf('Postgres')
         const findProjectName = pkg.indexOf(projectName)
         const findProjectDescription = pkg.indexOf(projectDescription)
 
         assert.typeOf(pkg, 'string')
+        assert.isAbove(findProjectDatabase, -1)
         assert.isAbove(findProjectName, -1)
         assert.isAbove(findProjectDescription, -1)
     })
