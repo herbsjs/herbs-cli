752c72548dc487f7e6aa3fc605dbfeae06c19d25
othonvv <99425672+othonvv@users.noreply.github.com>
2022-07-07 13:38:31 -0300

    Fixing readme test

--- src/tests/readme.test.js
+++ src/tests/readme.test.js
@@ -6,25 +6,16 @@ const path = require('path')
 const projectName = 'herbs-readme-test-runner'
 const projectDescription = 'I am test project description'
 
-const linkNpm = () => system.run(`cd bin && npm link --force`)
-const unlinkNpm = () => system.run(`cd bin && npm unlink --force @herbsjs/herbs-cli -g`)
-const npmInstall = () => system.run(`cd ${projectName} && npm install`)
-
-const generateProject = async (extraCMDs = '') => {
-    system.run(`herbs new --name ${projectName} --description "${projectDescription}" --author herbs --license MIT --graphql --rest --database postgres --npmInstall no ${extraCMDs}`)
-}
+const generateProject = (extraCMDs = '') => system.run(`herbs new --name ${projectName} --description "${projectDescription}" --author herbs --license MIT --graphql --rest --database postgres --npmInstall no ${extraCMDs}`)
 
 describe('generates readme file', () => {
     afterEach(async () => {
         fs.rmSync(path.resolve(process.cwd(), `${projectName}`), { recursive: true })
-        await unlinkNpm()
       })
 
     it('must to use custom user data', async () => {
 
-        await linkNpm()
         await generateProject()
-        await npmInstall()
 
         const pkg = filesystem.read(path.resolve(process.cwd(), `${projectName}/readme.md`))
 
