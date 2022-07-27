bc524682b4846f95d6afd15368ea340e47d06afd
danielsous <danieldelphi2013@gmail.com>
2022-07-27 02:55:46 -0300

    adjust test

--- src/tests/repositories.test.js
+++ src/tests/repositories.test.js
@@ -7,22 +7,19 @@ const path = require('path')
 
 const projectName = 'herbs-test-runner'
 
-const linknpm = () => system.run(`cd bin && npm link --force`)
-
 const generateProject = () => system.run(`herbs new --name ${projectName} --description "testing the herbs CLI"  --author herbs --license MIT --graphql --rest no --database postgres --npmInstall yes`)
-const callHerbsCli = () => system.run(`herbs -v && herbs`)
+const callHerbsCli = () => system.run(`herbs`)
 
 const herbsUpdate = () => system.run(`cd ${projectName} && herbs update`)
 
 describe('When I generate a complete project that uses postgres', () => {
-
-  it('must link npm herbs', async () => {
-      await linknpm()
-      await callHerbsCli()
+  afterEach(async () => {
+    fs.rmSync(path.resolve(process.cwd(), `${projectName}`), { recursive: true })
   })
 
-  it('Should I create a new entity with foreign key', async () => {
+  it('Should I create a new repository with foreign key', async () => {
     await generateProject()
+    await callHerbsCli()
     const customerEntity = 
     `
     const { entity, id, field } = require('@herbsjs/herbs')
@@ -45,9 +42,6 @@ describe('When I generate a complete project that uses postgres', () => {
     const dir = `${path.resolve(process.cwd(), `${projectName}/src/domain/entities/customer.js`)}`
     fs.writeFileSync(dir, customerEntity)
     expect(fs.existsSync(dir)).to.be.true
-  })
-
-  it('I hope to have a repository file for Customer entity after running Herbs Update', async () => {
     await herbsUpdate()
     expect(fs.existsSync(path.resolve(process.cwd(), `${projectName}/src/infra/data/repositories/customerRepository.js`))).to.be.true
   })
