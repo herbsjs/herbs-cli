81cb29e505888e101b2f5dd2b228988d48dca5e7
David Lojudice Sobrinho <dalssoft@gmail.com>
2022-11-11 19:33:57 -0300

    feat(spec): spec runs with or without use case info or metadata
    #169

--- src/commands/spec.js
+++ src/commands/spec.js
@@ -8,6 +8,25 @@ const cmd = {
     alias: ['s'],
     run: async toolbox => {
 
+        const undefinedGroup = "(default)"
+
+        function groupSteps(specs) {
+            const group = {}
+            specs.forEach(spec => {
+                groupName = spec.usecase ? spec.usecase : undefinedGroup
+                if (!group[groupName]) group[groupName] = []
+                group[groupName].push(spec)
+            })
+
+            // put undefinedGroup at the beginning
+            const orderedGroup = {}
+            orderedGroup[undefinedGroup] = group[undefinedGroup]
+            delete group[undefinedGroup]
+            Object.assign(orderedGroup, group)
+
+            return orderedGroup
+        }
+
         const herbarium = requireHerbarium("spec", toolbox.filesystem.cwd())
         const specs = herbarium.specs.all
 
@@ -15,44 +34,54 @@ const cmd = {
         const failed = 'failed'
         let errorCount = 0
 
-        for (const specInfo of Array.from(specs.values())) {
-            const spec = specInfo.spec
-            const ret = await spec.run()
-            const usecase = spec.usecase().description
-            const result = (ret) => ret !== failed ? green('🗸') : red('•')
-            const color = ret !== failed ? white : red
-            toolbox.print.info(`${color(usecase)} ${grey(italic('(spec)'))}`)
-            for (const scenario of spec.scenarios) {
-                toolbox.print.info(`   ${result(scenario.state)} ${white(scenario.description)} ${grey(italic('(scenario)'))}`)
-                if (scenario.state !== failed) continue
+        const usecasesGroup = groupSteps(specs)
 
-                for (const samples of scenario.samples) {
-                    for (const sample of samples.execution.scenarios) {
-                        const prettyPrint = (sample) => {
-                            for (const entrie of Object.entries(sample)) {
-                                toolbox.print.info(`        ${white(`${entrie[0]}: ${entrie[1]}`)}`)    
-                            }
-                        }
+        for (const groupName in usecasesGroup) {
+            let previousGroup = null
+            for (const specInfo of usecasesGroup[groupName]) {
 
-                        if (!samples.builtin) {
-                            toolbox.print.info(`   ${white(`${samples.description} ${grey(italic('(sample)'))}:`)}`)
-                            prettyPrint(sample.sample)
-                        }
-                        const steps = (step) => step.forEach(e => {
-                            const description = e.builtin ? `When run` : e.description
-                            const color = e.state !== failed ? grey : red
-                            const type = `(${e.type})`
-                            toolbox.print.info(`      ${result(e.state)} ${color(description)} ${grey(italic(type))}`)
-                            if (e.error) {
-                                toolbox.print.info(`\n          ${blue(e.error)} \n`)
-                                errorCount++
+                const spec = specInfo.spec
+                const ret = await spec.run()
+                const result = (ret) => ret !== failed ? green('🗸') : red('•')
+                const color = ret !== failed ? white : red
+                if (previousGroup !== groupName) {
+                    const name = groupName === undefinedGroup ? '' : groupName
+                    toolbox.print.info(`${color(name)} ${grey(italic('(spec)'))}`)
+                }
+                previousGroup = groupName
+
+                for (const scenario of spec.scenarios) {
+                    toolbox.print.info(`   ${result(scenario.state)} ${white(scenario.description)} ${grey(italic('(scenario)'))}`)
+                    if (scenario.state !== failed) continue
+
+                    for (const samples of scenario.samples) {
+                        for (const sample of samples.execution.scenarios) {
+                            const prettyPrint = (sample) => {
+                                for (const entrie of Object.entries(sample)) {
+                                    toolbox.print.info(`        ${white(`${entrie[0]}: ${entrie[1]}`)}`)
+                                }
                             }
-                        })
-                        steps(sample.givens)
-                        if (sample.stage === 'given') continue
-                        steps(sample.whens)
-                        if (sample.stage === 'when') continue
-                        steps(sample.checks)
+
+                            if (!samples.builtin) {
+                                toolbox.print.info(`   ${white(`${samples.description} ${grey(italic('(sample)'))}:`)}`)
+                                prettyPrint(sample.sample)
+                            }
+                            const steps = (step) => step.forEach(e => {
+                                const description = e.builtin ? `When run` : e.description
+                                const color = e.state !== failed ? grey : red
+                                const type = `(${e.type})`
+                                toolbox.print.info(`      ${result(e.state)} ${color(description)} ${grey(italic(type))}`)
+                                if (e.error) {
+                                    toolbox.print.info(`\n          ${blue(e.error)} \n`)
+                                    errorCount++
+                                }
+                            })
+                            steps(sample.givens)
+                            if (sample.stage === 'given') continue
+                            steps(sample.whens)
+                            if (sample.stage === 'when') continue
+                            steps(sample.checks)
+                        }
                     }
                 }
             }
