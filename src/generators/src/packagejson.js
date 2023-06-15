9338fd0d9fdfd65985c42687bf515df097980918
David Lojudice Sobrinho <dalssoft@gmail.com>
2023-06-15 11:12:45 -0300

    refactor(package.json): bump herbs libs version

--- src/generators/src/packagejson.js
+++ src/generators/src/packagejson.js
@@ -3,11 +3,11 @@ const { objToString } = require('../utils')
 const optionalPackages = {
   // TO DO: add "ˆ" to the herbs2knex and herbs2rest version when it is released
   mongo: ['"@herbsjs/herbs2mongo": "^3.0.3"', '"mongodb": "^4.8.1"'],
-  postgres: ['"@herbsjs/herbs2knex": "2.1.1-beta.1"', '"pg": "^8.7.3"'],
-  sqlite: ['"@herbsjs/herbs2knex": "2.1.1-beta.1"', '"sqlite3": "^5.0.11"'],
-  sqlserver: ['"@herbsjs/herbs2knex": "2.1.1-beta.1"', '"tedious": "^15.1.0"', '"mssql": "^9.0.1"'],
-  mysql: ['"@herbsjs/herbs2knex": "2.1.1-beta.1"', '"mysql2": "^2.3.3"'],
-  rest: ['"@herbsjs/herbs2rest": "4.1.0-beta.8"', '"express": "^4.18.1"', '"cors": "^2.8.5"'],
+  postgres: ['"@herbsjs/herbs2knex": "2.1.1-beta.3"', '"pg": "^8.7.3"'],
+  sqlite: ['"@herbsjs/herbs2knex": "2.1.1-beta.3"', '"sqlite3": "^5.0.11"'],
+  sqlserver: ['"@herbsjs/herbs2knex": "2.1.1-beta.3"', '"tedious": "^15.1.0"', '"mssql": "^9.0.1"'],
+  mysql: ['"@herbsjs/herbs2knex": "2.1.1-beta.3"', '"mysql2": "^2.3.3"'],
+  rest: ['"@herbsjs/herbs2rest": "4.1.0-beta.10"', '"express": "^4.18.1"', '"cors": "^2.8.5"'],
   graphql: ['"@herbsjs/herbs2gql": "^3.1.0-beta.1"', '"graphql": "^16.5.0"', '"apollo-server": "^3.8.2"','"apollo-server-express": "^3.8.2"', '"graphql-tools": "^8.2.12"', '"graphql-scalars": "^1.17.0"',]
 }
 
@@ -47,7 +47,7 @@ module.exports =
         : ''
 
       let packages = [
-        '"@herbsjs/herbs": "^2.1.0-beta.5"',
+        '"@herbsjs/herbs": "^2.1.0-beta.6"',
         '"@herbsjs/herbarium": "^1.5.0"',
         '"@herbsjs/herbsshelf": "^5.1.0-beta.5"',
         '"dotenv": "^16.0.1"',
